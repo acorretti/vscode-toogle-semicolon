6444ed98d8a1c43fcb7ea87acc8528927723f02d
Alfonso.Corretti <alfonso.corretti@dynatrace.com>
2018-06-23 08:58:26 +0200

    Placing characters right before line-comments at end of line.

--- src/extension.ts
+++ src/extension.ts
@@ -26,8 +26,15 @@ export function activate(context: ExtensionContext) {
 function toggle(lines: TextLine[], check: string = ';') {
     let firstToggle = -1; // -1 = not yet, 0 = remove, 1 = insert
     window.activeTextEditor.edit((editBuilder) => {
+        const commentRX = new RegExp(check+' *\/\/');
         lines.forEach(line => {
-            let found = line.text.slice(-1 * check.length) === check;
+            let found;
+            if (commentRX.test(line.text)) {
+                found = true;
+            }
+            else {
+                found = line.text.slice(-1 * check.length) === check;
+            }
             let shouldRemove = (follow && found && firstToggle !== 1) || (!follow && found);
             let shouldInsert = (follow && !found && firstToggle !== 0) || (!follow && !found);
             shouldRemove && remove(editBuilder, line, check);
@@ -38,16 +45,46 @@ function toggle(lines: TextLine[], check: string = ';') {
 }
 
 function remove(editBuilder: vscode.TextEditorEdit, line: TextLine, check: string) {
-    const lastStrRange = line.range.with(line.range.end.with(
-        line.range.end.line,
-        line.range.end.character - check.length)
-    );
+    let lastStrRange;
+    const commentRX = new RegExp('//([^"\'`]{'+check.length+'}) *\/\/.*');
+    if (commentRX.test(line.text)) {
+        const commentMatch = commentRX.exec(line.text);
+        lastStrRange = line.range.with(
+            line.range.end.with(
+                line.range.end.line,
+                line.range.end.character - commentMatch[0].length
+            ),
+            line.range.end.with(
+                line.range.end.line,
+                line.range.end.character - commentMatch[0].length + check.length
+            )
+        );
+    }
+    else {
+        lastStrRange = line.range.with(
+            line.range.end.with(
+                line.range.end.line,
+                line.range.end.character - check.length
+            )
+        );
+    }
     editBuilder.delete(lastStrRange);
 }
 
 function insert(editBuilder: vscode.TextEditorEdit, line: TextLine, check: string) {
-    editBuilder.insert(line.range.end, check);
+    const commentRX = new RegExp('([^"\'`]) *\/\/.*');
+    if (commentRX.test(line.text)) {
+        const commentMatch = commentRX.exec(line.text);
+        editBuilder.insert(line.range.end.with(
+            line.range.end.line,
+            line.range.end.character - commentMatch[0].length + 1
+        ), check);
+    }
+    else {
+        editBuilder.insert(line.range.end, check);
+    }
 }
+
 function getLines(): TextLine[] {
     let lines = [];
     window.activeTextEditor.selections
